47244a621bec18d5fa1f882a0a24c2f1ff3a9c96
FreakyCoder <kurayogun@gmail.com>
2022-07-28 16:32:39 +0300

    feat: dynamic search bar

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SearchBar from "react-native-dynamic-search-bar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <SearchBar
+        placeholder="Search here"
+        onPress={() => alert("onPress")}
+        onChangeText={(text) => console.log(text)}
+      />
     </View>
   );
 }
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
